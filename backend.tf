71eab77c949746c342293605360f4f2350bef905
ulan <umanasov24@gmail.com>
2020-01-30 17:03:47 -0600

    added s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
-bucket = "vpv-creation"
+bucket = "ulan-abi"
 key = "default/us-east-2/state/dev/infrastructure.tfstate"
-region = "us-east-2"
+region = "eu-west-1"
 }
 }
